106f23b785cd8dde989ce97cf678dcb3bfda9ffd
SHAFIN <mdshafis@metropolia.fi>
2024-09-12 14:08:24 +0300

    Initial Commit

--- Module 5 Exercise.py
+++ Module 5 Exercise.py
@@ -1,19 +1,53 @@
-def pizzaPurchase():
-    pizza1 = float(input("Enter the pizza price 1: "))
-    pizza2 = float(input("Enter the pizza price 2: "))
-    pd1 = float(input("Enter the pizza diameter 1: "))
-    pd2 = float(input("Enter the pizza diameter 2: "))
-
-    area1 = (3.14 / 4) * pd1 ** 2
-    area2 = (3.14 / 4) * pd2 ** 2
-
-    price1 = pizza1 / area1
-    price2 = pizza2 / area2
-
-    if price1 > price2:
-        print(f"You sould purchase pizza 2 bcz it is {price2:.2f} per sqr mtr.")
-    elif price2 > price1:
-        print(f"You sould purchase pizza 1 bcz it is {price1:.2f} per sqr mtr.")
+# #Exercise 01
+#
+# import random
+#
+# num_dice = int(input("How many dice would you like to roll: "))
+# total_sum = 0
+# for _ in range(num_dice):
+#     roll = random.randint(1, 6)
+#     total_sum += roll
+#
+#     print(f"The sum of your dice rolls is: {total_sum}")
 
 
-pizzaPurchase()
+# #Exercise 02:
+#
+# numbers = []
+#
+# while True:
+#     user_input = input("Enter a number (or press Enter to quit): ")
+#     if user_input == "":
+#         break
+#     try:
+#         number = float(user_input)
+#         numbers.append(number)
+#     except ValueError:
+#         print("Invalid input. Please enter a valid number.")
+#
+# numbers.sort(reverse=True)
+# print("The five greatest numbers are: ")
+# for number in numbers[:5]:
+#     print(number)
+
+
+
+# #Exercise 03:
+#
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     for i in range(2, int(num ** 0.5) + 1):  # Check divisibility up to the square root of the number
+#         if num % i == 0:
+#             return False
+#     return True
+#
+# user_input = input("Enter an integer: ")
+# try:
+#     number = int(user_input)
+#     if is_prime(number):
+#         print(f"{number} is a prime number.")
+#     else:
+#         print(f"{number} is not a prime number.")
+# except ValueError:
+#     print("Please enter a valid integer.")
