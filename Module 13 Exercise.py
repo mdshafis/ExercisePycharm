15279f0a54a71f12e19937b5ad46457af7fedcea
mdshafis <mdshafis@metropolia.fi>
2024-12-08 21:58:59 +0200

    Update Module 13 Exercise.py

--- Module 13 Exercise.py
+++ Module 13 Exercise.py
@@ -1,6 +1,4 @@
-#Implement a Flask backend service that tells whether a number received as a parameter is a prime number or not. Use the
-# prior prime number exercise as a starting point. For example, a GET request for number 31 is given
-# as: http://127.0.0.1:5000/prime_number/31. The response must be in the format of {"Number":31, "isPrime":true}.
+
 '''
 from flask import Flask, request, jsonify
 app = Flask(__name__)
@@ -18,10 +16,7 @@ def calculate_prime(num):
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
 '''
-#Implement a backend service that gets the ICAO code of an airport and then returns the name and location of the airport
-#in JSON format. The information is fetched from the airport database used on this course. For example, the GET request
-#for EFHK would be: http://127.0.0.1:5000/airport/EFHK. The response must be in the format of:
-# {"ICAO":"EFHK", "Name":"Helsinki-Vantaa Airport", "Location":"Helsinki"}.
+
 
 from flask import Flask, jsonify
 import mysql.connector
